5c4665df287bfba8c47648092c93c84a4c17bd62
Bastien <bastien.teinturier@outlook.com>
2017-03-22 13:10:22 +0100

    Adding score method

---
+++ 2017-03-22/BowlingGameKata/BowlingGameKata.UnitTests/BowlingGameShould.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata.UnitTests
+{
+    [TestClass]
+    public class BowlingGameShould
+    {
+        [TestMethod]
+        public void ReturnScore()
+        {
+            int score = new BowlingGame().Score();
+            Assert.AreEqual(0, score);
+        }
+    }
+}
