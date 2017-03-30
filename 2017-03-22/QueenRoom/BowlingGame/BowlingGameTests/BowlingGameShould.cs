452c4ccaa06d18d79cb1a1350fd6f39f79cf27cc
Thibault Francois <thfranco@microsoft.com>
2017-03-30 21:22:47 +0200

    Add test project

---
+++ 2017-03-22/QueenRoom/BowlingGame/BowlingGameTests/BowlingGameShould.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTests
+{
+    using BowlingGame;
+
+    [TestClass]
+    public class BowlingGameShould
+    {
+    }
+}
