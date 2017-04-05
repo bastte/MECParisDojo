5abf2a9c093286333b0764ab56a981ba5e81179e
Bastien <bastien.teinturier@outlook.com>
2017-04-05 15:44:30 +0200

    Add acceptance tests

--- 2017-04-05/Batcave/MineSweeperKata/MineSweeperShould.cpp
+++ 2017-04-05/Batcave/MineSweeperKata/MineSweeperShould.cpp
@@ -1,17 +1,62 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include <vector>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+using namespace std;
+
 namespace MineSweeperKata
-{		
+{
 	TEST_CLASS(MineSweeperShould)
 	{
 	public:
-		
-		TEST_METHOD(FailSinceWeHaventStartedYet)
+
+		TEST_METHOD(PassFirstAcceptanceTest)
 		{
-			Assert::Fail();
+			vector<vector<bool>> mineField
+			{
+				{ true, false, false, false },
+				{ false, false, false, false },
+				{ false, true, false, false },
+				{ false, false, false, false }
+			};
+
+			MineSweeper sweeper;
+			vector<vector<int>> annotatedMineField = sweeper.Annotate(mineField);
+
+			vector<vector<int>> expectedAnnotatedMineField
+			{
+				{42, 1, 0, 0},
+				{2, 2, 1, 0},
+				{1, 42, 1, 0},
+				{1, 1, 1, 0}
+			};
+
+			Assert::AreEqual(expectedAnnotatedMineField, annotatedMineField);
+		}
+
+		TEST_METHOD(PassSecondAcceptanceTest)
+		{
+			vector<vector<bool>> mineField
+			{
+				{ true, true, false, false, false },
+				{ false, false, false, false, false },
+				{ false, true, false, false, false }
+			};
+
+			MineSweeper sweeper;
+			vector<vector<int>> annotatedMineField = sweeper.Annotate(mineField);
+
+			vector<vector<int>> expectedAnnotatedMineField
+			{
+				{ 42, 42, 1, 0, 0 },
+				{ 3, 3, 2, 0, 0 },
+				{ 1, 42, 1, 0, 0 }
+			};
+
+			Assert::AreEqual(expectedAnnotatedMineField, annotatedMineField);
 		}
 
 	};
