c149ba8b40a932ef6168901b2349698f027ad987
Bastien <bastien.teinturier@outlook.com>
2017-04-14 13:16:57 +0200

    Advanced rule modelling

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -10,9 +10,14 @@ namespace DiceScorer
     {
         public int Score(List<int> dice)
         {
-            int count = dice.Count(x => x == 1);
-                
-            return count == 1 ? 100 : 0;
+            int[] count = Enumerable.Range(1, 6).Select(value => dice.Count(x => x == value)).ToArray();
+
+            int score = 0;
+            if (count[1-1] == 1)
+                score += 100;
+            if (count[5-1] == 1)
+                score += 50;
+            return score;
         }
     }
 }
--- 2017-04-13/bastte/DiceScorer/DiceScorerShould.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorerShould.cs
@@ -16,11 +16,35 @@ namespace DiceScorer
         }
 
         [TestMethod]
-        public void CorrectlyScoreASingleDiceWithPoints()
+        public void CorrectlyScoreASingleOneDiceWithPoints()
         {
             var scorer = new DiceScorer();
             int score = scorer.Score(new List<int> { 1 });
             Assert.AreEqual(100, score);
+        }
+
+        [TestMethod]
+        public void CorrectlyScoreASingleFiveDiceWithPoints()
+        {
+            var scorer = new DiceScorer();
+            int score = scorer.Score(new List<int> { 5 });
+            Assert.AreEqual(50, score);
+        }
+
+        [TestMethod]
+        public void CorrectlyScoreASingleOneAndASingleFiveDiceWithPoints()
+        {
+            var scorer = new DiceScorer();
+            int score = scorer.Score(new List<int> { 1, 5 });
+            Assert.AreEqual(150, score);
+        }
+
+        [TestMethod]
+        public void CorrectlyScoreASampleSinglesSequenceWithPoints()
+        {
+            var scorer = new DiceScorer();
+            int score = scorer.Score(new List<int> { 1, 2, 3, 5});
+            Assert.AreEqual(150, score);
         }
     }
 }
