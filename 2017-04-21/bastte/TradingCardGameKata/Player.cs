165501a6b5c9c7bbf1d8b20fefe6bee4f72f101d
Bastien <bastien.teinturier@outlook.com>
2017-04-21 12:45:53 +0200

    Player starts with 30 health

--- 2017-04-21/bastte/TradingCardGameKata/Player.cs
+++ 2017-04-21/bastte/TradingCardGameKata/Player.cs
@@ -1,11 +1,16 @@
-﻿namespace TradingCardGameKata
+﻿using System.Collections.Generic;
+
+namespace TradingCardGameKata
 {
     internal class Player
     {
         public int ManaSlots { get; }
+        public int Health { get; }
 
         public Player()
         {
+            ManaSlots = 0;
+            Health = 30;
         }
     }
 }
--- 2017-04-21/bastte/TradingCardGameKata/TradingCardGameShould.cs
+++ 2017-04-21/bastte/TradingCardGameKata/TradingCardGameShould.cs
@@ -11,5 +11,12 @@ namespace TradingCardGameKata
             var player = new Player();
             Assert.Equal(0, player.ManaSlots);
         }
+
+        [Fact]
+        public void PlayerStarsWith30Health()
+        {
+            var player = new Player();
+            Assert.Equal(30, player.Health);
+        }
     }
 }
